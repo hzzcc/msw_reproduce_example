0b258b06470e0180fdf394db48b502f3559a6c0b
黄铮铮 <huangzhengzheng@metrodata.cn>
2021-11-26 10:43:26 +0800

    feat: 🎸 msw mock /user

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  console.log("render");
+  React.useEffect(() => {
+    fetch("/user")
+      .then((resp) => resp.json())
+      .then((resp) => {
+        console.log("resp: ", resp);
+      });
+  }, []);
+
+  const srcDoc = `!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <style>root,html,body{width:100%;height:100%;margin:0;padding:0;overflow:hidden;}</style>
+    </head>
+    <body>
+    <script>
+      fetch("/user").then((resp) => resp.json())
+      .then((resp) => {
+        console.log("request in frame: ", resp);
+      }).catch((err) =>{
+        console.error("request in frame error::", err);
+      });
+    </script>
+    </body>
+  </html>`;
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +46,7 @@ function App() {
         >
           Learn React
         </a>
+        <iframe title="frame" srcDoc={srcDoc} />
       </header>
     </div>
   );
